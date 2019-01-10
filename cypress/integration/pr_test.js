f846fc5eed70aadfffa2e5d632635db867a1a34a
Vibhor Sehgal <nurvs@nus.edu.sg>
2019-01-10 19:45:29 +0530

    Minor changes

--- cypress/integration/pr_test.js
+++ cypress/integration/pr_test.js
@@ -1,7 +1,9 @@
 describe("UI Pre-Commit Test", function() {
   beforeEach(function() {
     cy.visit("/");
-    /*
+  });
+
+  it("Visits Website and Logs in using Dummy details", function() {
     cy.get("#bluegenes-main-nav").within(() => {
       cy.get("ul")
         .find("li.dropdown.mine-settings.secondary-nav")
@@ -9,10 +11,6 @@ describe("UI Pre-Commit Test", function() {
         .contains("FlyMine")
         .click();
     });
-    */
-  });
-
-  it("Visits Website and Logs in using Dummy details", function() {
     cy.contains("Log In").click();
     cy.get("#email").type("demo@intermine.org");
     cy.get("#password").type("demo");
@@ -33,10 +31,28 @@ describe("UI Pre-Commit Test", function() {
     cy.get("button")
       .contains("Search")
       .click();
-    cy.get(".results-summary").should("have", "Results");
     cy.wait("@getData");
     cy.get("@getData").should(xhr => {
         expect(xhr.status).to.equal(200);
     });
+    cy.get(".results-summary").should("have", "Results");
+  });
+
+  it("Perform a region search using existing example", function() {
+    cy.server();
+    cy.route("POST", "*/service/query/results").as("getData");
+    cy.contains("Regions").click();
+    cy.get(".guidance")
+      .contains("[Show me an example]")
+      .click();
+    cy.get(".region-text > .form-control").should("not.be.empty");
+    cy.get("button")
+      .contains("Search")
+      .click();
+    cy.wait("@getData");
+    cy.get("@getData").should(xhr => {
+        expect(xhr.status).to.equal(200);
+    });
+    cy.get(".results-summary").should("have", "Results");
   });
 });
